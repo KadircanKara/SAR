6ed28e103692090c9f99865be138056df25e06ee
Kadircan Kara <86204716+KadircanKara@users.noreply.github.com>
2025-01-07 14:00:04 +0300

    Add files via upload

--- WeightedSum.py
+++ WeightedSum.py
@@ -10,4 +10,12 @@ def mission_time_and_percentage_connectivity_and_max_mean_tbv_ws(sol:PathSolutio
     return sol.mission_time/3 - sol.percentage_connectivity/3 + sol.max_mean_tbv/3
 
 def mission_time_and_percentage_connectivity_and_max_disconnected_time_and_mean_disconnected_time_and_max_mean_tbv_ws(sol:PathSolution):
-    return sol.mission_time/5 - sol.percentage_connectivity/5 + sol.max_disconnected_time/5 + sol.mean_disconnected_time/5 + sol.max_mean_tbv/5
+    # Normalize Required Objectives
+    normalized_mission_time = float(f"0.{str(round(sol.mission_time))}")
+    normalized_max_disconnected_time = float(f"0.{str(round(sol.max_disconnected_time))}")
+    normalized_max_mean_tbv = float(f"0.{str(round(sol.max_mean_tbv))}")
+    # Sum Objectives (Normalized and otherwise)
+    normalized_objective_value = normalized_mission_time - sol.percentage_connectivity + normalized_max_disconnected_time + sol.mean_disconnected_time + normalized_max_mean_tbv
+    # print(f"Normalized Objective Value: {normalized_objective_value}")
+    return normalized_objective_value
+    # return sol.mission_time/5 - sol.percentage_connectivity/5 + sol.max_disconnected_time/5 + sol.mean_disconnected_time/5 + sol.max_mean_tbv/5
